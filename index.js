c090a89d7ae3cc88c334d080070ec05b65804407
Mark Angelo Roxas <markangeloroxas@Marks-iMac.local>
2017-11-27 03:13:02 +0800

    Shopify Admin API

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
-
+var request = require("request");
 const puppeteer = require('puppeteer');
 
 const selector = require('./configSelector');
@@ -10,7 +10,7 @@ const selector = require('./configSelector');
 async function run(urlstore, user, pword) {
 
     
-    let url = urlstore;
+    let url = 'https://' + urlstore + '.myshopify.com/admin';
     let username = user;
     let password = pword;
     // Debug
@@ -23,147 +23,106 @@ async function run(urlstore, user, pword) {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
 
-    async function adminLogin() {  
-            await page.goto(url);
-            console.log(selector.userfield);
-            await page.click(selector.userfield);
-            await page.keyboard.type(username);
-
-            await page.click(selector.pwfield);
-            await page.keyboard.type(password);
-
-            await page.click(selector.loginButton, { delay: 10000 });
-
-            await page.waitForNavigation();
-            console.log("Logged in successfully");
-    }
-
-
-    async function installTheme() {
-        
-        await page.goto(url + "/themes");
-        await page.click(selector.uploadTheme);
-        const fileUploaders = await page.$$("input[type=file]");
-        await page.waitFor(5000);
-        var filePath = __dirname + '/' + selector.themeZipfile;
-        console.log("filePath:" + filePath);
-        var inputfileup = await page.$("input[type=file]");
-        await inputfileup.uploadFile(filePath);
-        await page.waitFor(2000);
-        await page.click(selector.uploadButton);
-        await page.waitFor(60000);
     
-        var actionspop = await page.$(selector.firstActions);
-        await actionspop.click();
-        await page.waitFor(2000);
-        var publish = await page.$(selector.firstActionsPublilsh);
-        await publish.click();
-        await page.waitFor(2000);
-        var confirm = await page.$(selector.confirmFirstActionPublish);
-        await confirm.click();
-        await page.waitFor(2000);
-        console.log("Theme uploaded and installed. Now creating pages");
-    }
+        async function adminLogin() {  
+                await page.goto(url);
+                console.log(selector.userfield);
+                await page.click(selector.userfield);
+                await page.keyboard.type(username);
     
-    async function gotoCheckoutSettings() {
-        await page.goto(url + '/settings/checkout');
-        await page.waitFor(2000);
-    }
+                await page.click(selector.pwfield);
+                await page.keyboard.type(password);
+    
+                await page.click(selector.loginButton, { delay: 10000 });
+    
+                await page.waitForNavigation();
+                console.log("Logged in successfully");
+        }
+    
 
-    async function getGeneratedText(button, textbody) {
-        var actionspop = await page.$(button);
-        await actionspop.click();
-        const nameElement = await page.$eval(textbody, function (heading) {
-            return heading.value;
-        })
-        return nameElement.replace(/(?:\r\n|\r|\n)/g, '<br />');
-    }
-
-    async function createNewPage(titleText, codeTxt) {
+    async function gotoApps() {
+        await page.goto(url + '/apps/private/new');
         await page.waitFor(2000);
-        await page.goto(url + '/pages/new');
-        await page.waitFor(2000);
-        var pageTitle = await page.$("#page-name");
-        await pageTitle.click();
-        await page.waitFor(2000);
-        await page.keyboard.type(titleText);
-        await page.waitFor(2000);
-        let toCode = await page.$("#page-content_parent > div.rte-toolbar.for-rich-view > div > div > div > button");
-        await toCode.click();
-        await page.waitFor(2000);
+        var codeTxt;
         await page.evaluate((codeTxt) => {
-            document.querySelector('#page-content').value = codeTxt;
+            document.getElementById("api_client_title").value = "Theme";
+            document.getElementById("api_client_contact_email").value = "markangelo@themillionairemastermind.com";
+           
+           document.getElementById("api_client[access_scope][content][authenticated]").value = "write_content";
+            document.getElementById("api_client[access_scope][themes][authenticated]").value = "write_themes";
+           document.getElementById("api_client[access_scope][products][authenticated]").value = "write_products";
+           document.getElementById("api_client[access_scope][product_listings][authenticated]").value = "write_product_listings";
+           $( "button:contains('Save')" )[0].click();
         }, codeTxt);
         await page.waitFor(2000);
-        let save = await page.$('[form="new_page"');
-        await save.click();
-        await page.waitFor(2000);
     }
 
-    async function createContactPage(titleText) {
-        await page.waitFor(2000);
-        await page.goto(url + '/pages/new');
-        await page.waitFor(2000);
-        var pageTitle = await page.$("#page-name");
-        await pageTitle.click();
-        await page.waitFor(2000);
-        await page.keyboard.type(titleText);
-        await page.waitFor(2000);
-        var codeTxt = "contact";
-        await page.evaluate((codeTxt) => {
-            document.querySelector('#page_template_suffix').value = codeTxt;
-        }, codeTxt);
-        await page.waitFor(2000);
-        let save = await page.$('[form="new_page"');
-        await save.click();
-        await page.waitFor(2000);
-    }
 
-    async function gotoPreferences() {
-        await page.goto(url);
-        await page.goto(url + '/online_store/preferences');
-        await page.waitFor(2000);
-    }
 
-    async function enableStore() {
-        var codeTxt = 0;
-        await page.evaluate((codeTxt) => {
-            console.log(document.querySelector("#shop_password_enabled").checked);
-            if (document.querySelector("#shop_password_enabled").checked) {
-                document.querySelector("#shop_password_enabled").checked = false;
+    async function getAPI() {
+        await page.waitFor(2000);
+        const result = await page.evaluate(() => {
+            let userapi = document.querySelector('#api_key').value;
+            let paswordapi = document.querySelector('#private_app_password').value;
+    
+            return {
+                userapi,
+                paswordapi
             }
-        }, codeTxt);
-        await page.click(selector.saveEnableStore);
-    }
+    
+        });
 
+        console.log(result.userapi);
+        console.log(result.paswordapi);
+
+
+        shopifyCreatePage("POST", "pages.json", urlstore, result.userapi, result.paswordapi, "FAQ", selector.htmlFAQ);
+        shopifyCreatePage("POST", "pages.json", urlstore, result.userapi, result.paswordapi, "PRIVACY POLICY", selector.htmlPrivatePolicy);
+        shopifyCreatePage("POST", "pages.json", urlstore, result.userapi, result.paswordapi, "RETURN POLICY", selector.htmlReturnPolicy);
+        shopifyCreatePage("POST", "pages.json", urlstore, result.userapi, result.paswordapi, "TERMS OF SERVICES", selector.htmlTermsofService);
+        shopifyCreatePage("POST", "pages.json", urlstore, result.userapi, result.paswordapi, "CONTACT US", "", "contact");
+        shopifyInstallTheme("POST", "themes.json", urlstore, result.userapi, result.paswordapi);
+        await page.waitFor(15000);
+    }
+    
 
     await adminLogin();
-    await installTheme();
-
-    await page.waitFor(2000);
-    await gotoCheckoutSettings();   //to get generated text
-    await page.waitFor(2000);
-    let refundpage = await getGeneratedText(selector.generateSampleRefundPolicy, selector.getSampleRefundPolicyText);
-    await page.waitFor(2000);
-    let privacypage = await getGeneratedText(selector.generateSamplePrivacyPolicy, selector.getSamplePrivacyPolicyText);
-    await page.waitFor(2000);
-
-    let termspage = await getGeneratedText(selector.generateTermsOfService, selector.getTermsOfServiceText);
-    await page.waitFor(2000);
-    await createNewPage("Shipping Returns and Exchanges", refundpage);
-    console.log("Shipping Returns and Exchanges page done");
-    await createNewPage("Privacy Policy", privacypage);
-    console.log("Privacy Policy page done");
-    await createNewPage("Terms of Service", termspage);
-    await createContactPage("Contact Us");
-    console.log("Contact Us page done");
-    await page.waitFor(2000);
-    await gotoPreferences();
-    console.log("Enabling Store");
-    await enableStore();
-    console.log("Finished");
+    await gotoApps();
+    await getAPI();
+  
     browser.close();
 }
+
+
+    function shopifyCreatePage(method, pathapi, storeurl, userapi, paswordapi, title, body, template) {
+        var options = {
+            method: method, url: 'https://' + userapi + ':' + paswordapi + '@' + storeurl + '.myshopify.com/admin/' + pathapi, body:{
+                    page:{
+                            title: title,
+                            body_html: body,
+                            template_suffix: template,
+                            published: true
+                        }
+                },
+            json: true
+        };
+        request(options, function (error, response, body) {
+            if (error) throw new Error(error);
+            console.log(body);
+        });
+    }
+    function shopifyInstallTheme(method, pathapi, storeurl, userapi, paswordapi) {
+        var options = {
+            method: method, url: 'https://' + userapi + ':' + paswordapi + '@' + storeurl + '.myshopify.com/admin/' + pathapi, body:{ theme: 
+            { name: 'TheMMThemeAutoInstall',
+              src: 'http://themillionairemastermind.com/SCA/TheMillionaireMastermindTheme.zip',
+              role: 'main' } },
+        json: true };
+        request(options, function (error, response, body) {
+            if (error) throw new Error(error);
+            console.log(body);
+        });
+    }
 
 
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -173,7 +132,7 @@ app.get('/',function(req,res){
   res.sendfile("./src/index.html");
 });
 app.post('/login',function(req,res){
-  var urlstore=req.body.urlstore;
+  var urlstore= req.body.urlstore;
   var user_name=req.body.user;
   var password=req.body.password;
   console.log("User name = "+user_name+", password is "+password);
--- test.js
+++ test.js
@@ -1,37 +1,123 @@
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+
 const puppeteer = require('puppeteer');
-const url = "https://shop-mermaid-blanket.myshopify.com/admin/";
-const userfield = '#Login';
-const pwfield = '#Password';
-const loginButton = "#LoginSubmit";
-const username = "abdullahtheecom@gmail.com";
-const password = "Abdullah1!";
-const uploadTheme = "#themes-index > section:nth-child(4) > div > div.ui-annotated-section__annotation > div.ui-annotated-section__description > button";
-const chooseFile = "#upload_theme_form > div.ui-modal__body > div > div > div:nth-child(5) > input[type=\"file\"]:nth-child(2)";
-const uploadButton = "#upload_theme_form > div.ui-modal__footer > div > div.ui-modal__primary-actions > div > button";
 
+const selector = require('./configSelector');
 
-var generateSampleRefundPolicy = "#settings-shop-policies > div.ui-annotated-section__content > section > div:nth-child(1) > div > div.next-input-wrapper > div > button";
-var getSampleRefundPolicyText = "#shop_refund-policy";
+async function run(urlstore, user, pword) {
 
-var generateSamplePrivacyPolicy = "#settings-shop-policies > div.ui-annotated-section__content > section > div:nth-child(2) > div > div.next-input-wrapper > div > button";
-var getSamplePrivacyPolicyText = "#shop_privacy-policy";
+    
+    let url = urlstore;
+    let username = user;
+    let password = pword;
+    // Debug
 
-var generateTermsOfService = "#settings-shop-policies > div.ui-annotated-section__content > section > div:nth-child(3) > div > div.next-input-wrapper > div > button";
-var getTermsOfServiceText = "#shop_terms-of-service";
-var enableCheckBox = "#shop_password_enabled";
-var saveEnableStore = ".ui-button.ui-button--primary.js-btn-loadable.js-btn-primary.ui-context-bar__actions-primary";
-
-async function run() {
-    const browser = await puppeteer.launch({
-        headless: false,
-        devtools: true,
-        userDataDir: "~/Library/Application Support/Google/Chrome"
-
-    });
+    // const browser = await puppeteer.launch({
+    //     headless: false,
+    //     devtools: true,
+    //     userDataDir: "~/Library/Application Support/Google/Chrome"
+    // });
+    const browser = await puppeteer.launch();
     const page = await browser.newPage();
 
+    async function adminLogin() {  
+            await page.goto(url);
+            console.log(selector.userfield);
+            await page.click(selector.userfield);
+            await page.keyboard.type(username);
+
+            await page.click(selector.pwfield);
+            await page.keyboard.type(password);
+
+            await page.click(selector.loginButton, { delay: 10000 });
+
+            await page.waitForNavigation();
+            console.log("Logged in successfully");
+    }
 
 
+    async function installTheme() {
+        
+        await page.goto(url + "/themes");
+        await page.click(selector.uploadTheme);
+        const fileUploaders = await page.$$("input[type=file]");
+        await page.waitFor(5000);
+        var filePath = __dirname + '/' + selector.themeZipfile;
+        console.log("filePath:" + filePath);
+        var inputfileup = await page.$("input[type=file]");
+        await inputfileup.uploadFile(filePath);
+        await page.waitFor(2000);
+        await page.click(selector.uploadButton);
+        await page.waitFor(60000);
+    
+        var actionspop = await page.$(selector.firstActions);
+        await actionspop.click();
+        await page.waitFor(2000);
+        var publish = await page.$(selector.firstActionsPublilsh);
+        await publish.click();
+        await page.waitFor(2000);
+        var confirm = await page.$(selector.confirmFirstActionPublish);
+        await confirm.click();
+        await page.waitFor(2000);
+        console.log("Theme uploaded and installed. Now creating pages");
+    }
+    
+    async function gotoCheckoutSettings() {
+        await page.goto(url + '/settings/checkout');
+        await page.waitFor(2000);
+    }
+
+    async function getGeneratedText(button, textbody) {
+        var actionspop = await page.$(button);
+        await actionspop.click();
+        const nameElement = await page.$eval(textbody, function (heading) {
+            return heading.value;
+        })
+        return nameElement.replace(/(?:\r\n|\r|\n)/g, '<br />');
+    }
+
+    async function createNewPage(titleText, codeTxt) {
+        await page.waitFor(2000);
+        await page.goto(url + '/pages/new');
+        await page.waitFor(2000);
+        var pageTitle = await page.$("#page-name");
+        await pageTitle.click();
+        await page.waitFor(2000);
+        await page.keyboard.type(titleText);
+        await page.waitFor(2000);
+        let toCode = await page.$("#page-content_parent > div.rte-toolbar.for-rich-view > div > div > div > button");
+        await toCode.click();
+        await page.waitFor(2000);
+        await page.evaluate((codeTxt) => {
+            document.querySelector('#page-content').value = codeTxt;
+        }, codeTxt);
+        await page.waitFor(2000);
+        let save = await page.$('[form="new_page"');
+        await save.click();
+        await page.waitFor(2000);
+    }
+
+    async function createContactPage(titleText) {
+        await page.waitFor(2000);
+        await page.goto(url + '/pages/new');
+        await page.waitFor(2000);
+        var pageTitle = await page.$("#page-name");
+        await pageTitle.click();
+        await page.waitFor(2000);
+        await page.keyboard.type(titleText);
+        await page.waitFor(2000);
+        var codeTxt = "contact";
+        await page.evaluate((codeTxt) => {
+            document.querySelector('#page_template_suffix').value = codeTxt;
+        }, codeTxt);
+        await page.waitFor(2000);
+        let save = await page.$('[form="new_page"');
+        await save.click();
+        await page.waitFor(2000);
+    }
 
     async function gotoPreferences() {
         await page.goto(url);
@@ -47,20 +133,56 @@ async function run() {
                 document.querySelector("#shop_password_enabled").checked = false;
             }
         }, codeTxt);
-        await page.click(saveEnableStore);
+        await page.click(selector.saveEnableStore);
     }
 
-await gotoPreferences();
 
-await enableStore();
+    await adminLogin();
+    await installTheme();
 
+    await page.waitFor(2000);
+    await gotoCheckoutSettings();   //to get generated text
+    await page.waitFor(2000);
+    let refundpage = await getGeneratedText(selector.generateSampleRefundPolicy, selector.getSampleRefundPolicyText);
+    await page.waitFor(2000);
+    let privacypage = await getGeneratedText(selector.generateSamplePrivacyPolicy, selector.getSamplePrivacyPolicyText);
+    await page.waitFor(2000);
 
-
-
-
-
-
+    let termspage = await getGeneratedText(selector.generateTermsOfService, selector.getTermsOfServiceText);
+    await page.waitFor(2000);
+    await createNewPage("Shipping Returns and Exchanges", refundpage);
+    console.log("Shipping Returns and Exchanges page done");
+    await createNewPage("Privacy Policy", privacypage);
+    console.log("Privacy Policy page done");
+    await createNewPage("Terms of Service", termspage);
+    await createContactPage("Contact Us");
+    console.log("Contact Us page done");
+    await page.waitFor(2000);
+    await gotoPreferences();
+    console.log("Enabling Store");
+    await enableStore();
+    console.log("Finished");
+    browser.close();
 }
 
-run();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.get('/',function(req,res){
+  res.sendfile("./src/index.html");
+});
+app.post('/login',function(req,res){
+  var urlstore=req.body.urlstore;
+  var user_name=req.body.user;
+  var password=req.body.password;
+  console.log("User name = "+user_name+", password is "+password);
+  run(urlstore, user_name, password);
+  res.end("yes");
+});
+app.listen(3000,function(){
+    console.log(selector.firstActions);
+  console.log("Started on PORT 3000");
+})
+
 
